11cc4b4d05df8496e9f3f81330b871a61d87ba96
EdConFCE <103198594+EdConFCE@users.noreply.github.com>
2022-04-12 12:24:28 -0500

    adición de menú para vista de celular

---
+++ menu.js
@@ -0,0 +1,10 @@
+function mostrar(){
+    document.getElementById('abrirMenu').style.visibility='hidden';
+    document.getElementById('menu').style.visibility='visible';
+    document.getElementById('cerrarMenu').style.visibility='visible';
+}
+function ocultar(){
+    document.getElementById('abrirMenu').style.visibility='visible';
+    document.getElementById('menu').style.visibility='hidden';
+    document.getElementById('cerrarMenu').style.visibility='hidden';
+}
